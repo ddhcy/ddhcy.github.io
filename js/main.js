f2c8bb46f0e47f870f0a10cb09fe58b16182fd73
sutaicha <yutao@dadehecy.com>
2023-07-14 12:39:15 +0800

    20230714

--- js/main.js
+++ js/main.js
@@ -671,5 +671,59 @@
         ssMoveTo();
 
     })();
+    /*map*/
+       !function(){
+         var infoWindow, map, level = 12,
+           center = {lng: 113.746719, lat: 29.586781},
+           features = [{"icon":"pot","color":"red","name":"大德和茶业工厂","desc":"湖北省赤壁市赵李桥镇砖茶产业园（G107国道与渣枫线交汇处）","lnglat":{"Q":29.585437327545623,"R":113.74723401010033,"lng":113.747234,"lat":29.585437},"offset":{"x":-9,"y":-31},"type":"Marker"}];
+
+         function loadFeatures(){
+           for(var feature, data, i = 0, len = features.length, j, jl, path; i < len; i++){
+             data = features[i];
+             switch(data.type){
+               case "Marker":
+                 feature = new AMap.Marker({ map: map, position: new AMap.LngLat(data.lnglat.lng, data.lnglat.lat),
+                   zIndex: 3, extData: data, offset: new AMap.Pixel(data.offset.x, data.offset.y), title: data.name,
+                   content: '<div class="icon icon-' + data.icon + ' icon-'+ data.icon +'-' + data.color +'"></div>' });
+                 break;
+               case "Polyline":
+                for(j = 0, jl = data.path.length, path = []; j < jl; j++){
+    							path.push(new AMap.LngLat(data.path[j].lng, data.path[j].lat));
+    						}
+                 feature = new AMap.Polyline({ map: map, path: path, extData: data, zIndex: 2,
+                   strokeWeight: data.strokeWeight, strokeColor: data.strokeColor, strokeOpacity: data.strokeOpacity });
+                 break;
+               case "Polygon":
+                 for(j = 0, jl = data.path.length, path = []; j < jl; j++){
+                   path.push(new AMap.LngLat(data.path[j].lng, data.path[j].lat));
+                 }
+                 feature = new AMap.Polygon({ map: map, path: path, extData: data, zIndex: 1,
+                   strokeWeight: data.strokeWeight, strokeColor: data.strokeColor, strokeOpacity: data.strokeOpacity,
+                   fillColor: data.fillColor, fillOpacity: data.fillOpacity });
+                 break;
+               default: feature = null;
+             }
+             if(feature){ AMap.event.addListener(feature, "click", mapFeatureClick); }
+           }
+         }
+
+         function mapFeatureClick(e){
+           if(!infoWindow){ infoWindow = new AMap.InfoWindow({autoMove: true,isCustom: false}); }
+           var extData = e.target.getExtData();
+           infoWindow.setContent("<div class='myinfowindow'><p>" + extData.name + "</p><div>" + extData.desc + "</div></div>");
+           infoWindow.open(map, e.lnglat);
+         }
+
+         map = new AMap.Map("mapContainer", {center: new AMap.LngLat(center.lng, center.lat), level: level, keyboardEnable:true, dragEnable:true, scrollWheel:true, doubleClickZoom:true});
+
+         loadFeatures();
+
+         map.on('complete', function(){
+           map.plugin(["AMap.ToolBar", "AMap.Scale"], function(){
+             map.addControl(new AMap.ToolBar({ruler: false, direction: false, locate: false})); map.addControl(new AMap.Scale);
+           });
+         })
+
+     	}();
 
 })(document.documentElement);
